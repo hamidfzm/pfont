1b7977ab29b9cc14580ce1a463e6ac3942dacf0f
Hamid FzM <hamidfzm@gmail.com>
2014-10-15 22:59:37 +0330

    adding favico. activating babel which is failed until now

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
 from flask import render_template
-
+from flask.ext.babel import gettext as _
 from app.utilis.decorators import title
 from . import mod
 
 
 @mod.route('/')
-@title('Resume')
+@title(_('Persian Libre Font Campaign'))
 def index():
     return render_template('index.html')
