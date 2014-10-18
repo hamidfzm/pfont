71715c7dd9e1c5a5af13fe2ab43ec23e5fe4f961
Hamid FzM <hamidfzm@gmail.com>
2014-10-18 14:47:03 +0330

    writing donate and donator model

--- app/main/models.py
+++ app/main/models.py
@@ -1,10 +1,16 @@
-from mongoengine import Document, StringField, EmailField, IntField
+from mongoengine import Document, StringField, EmailField, IntField, DateTimeField, ListField, EmbeddedDocument
+from datetime import datetime
+
+
+class Donate(Document):
+    amount = IntField(required=True, min_value=0)
+    date = DateTimeField(required=True, default=datetime.now())
 
 
 class Donator(Document):
     nickname = StringField(required=False, max_length=100)
     email = EmailField(required=True)
-    amount = IntField(required=True, min_value=0)
+    donate = ListField(EmbeddedDocument(Donate))
 
     meta = {
         'indexes': ['email']
