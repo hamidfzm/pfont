ad3b1aec568250a4dfe22a6a18ba26a05fbdd7b3
Hamid FzM <hamidfzm@gmail.com>
2014-10-19 23:38:54 +0330

    adding ajax_view decorator and donate view

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
 
 # flask import
-from flask import render_template
+from flask import render_template, request, jsonify
 from flask.ext.babel import gettext as _
 
 # project import
-from app.utilis.decorators import title
+from app.utilis.decorators import title, ajax_view
 from . import mod
 from .forms import DonatorForm
 
@@ -13,3 +13,13 @@ from .forms import DonatorForm
 @title(_('Persian Libre Font Campaign'))
 def index():
     return render_template('index.html', form=DonatorForm())
+
+
+@mod.route('donate/', methods=['POST'])
+@ajax_view
+def donate():
+    form = DonatorForm(request.form)
+
+    if form.validate():
+        return jsonify({'status': 1})
+    return jsonify({'status': 2, 'form': render_template('donate_form.html', form=form)})
--- app/utilis/decorators.py
+++ app/utilis/decorators.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import g
+from flask import g, request, abort
 
 
 def title(text):
@@ -10,3 +10,15 @@ def title(text):
             return f(*args, **kwargs)
         return decorator
     return decorator_arg
+
+
+def ajax_view(f):
+    """
+    Response only to ajax request other wise abort 404
+    """
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        if request.headers.get('X_REQUESTED_WITH') == 'XMLHttpRequest':
+            return f(*args, **kwargs)
+        return abort(404)
+    return decorator
