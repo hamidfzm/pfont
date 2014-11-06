63762f7c87e1812a916e6b296285d6ab4dbc6e7e
Hamid FzM <hamidfzm@gmail.com>
2014-11-06 10:48:30 +0330

    adding translation creator to babel shell script

--- app/babel.sh
+++ app/babel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pybabel extract -F babel.cfg -o messages.pot .
+pybabel init -i messages.pot -d translations -l fa
 pybabel update -i messages.pot -d translations
 pybabel compile -f -d translations
