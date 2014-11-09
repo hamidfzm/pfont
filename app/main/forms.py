d60fe0ba18e8723257d98505e4ceaf984c42422a
Ebrahim Byagowi <ebrahim@gnu.org>
2014-11-09 14:14:01 +0330

    Minor changes
    * Moved direction to HTML as an attribute http://stackoverflow.com/a/5375907
    * Improve preferred font chain also in email
    * Some other minor spell changes

--- app/main/forms.py
+++ app/main/forms.py
@@ -10,16 +10,16 @@ class DonatorForm(Form):
     email = StringField('Email', [
         validators.Length(min=6, max=100, message=u'طول پست الکترونیک باید بین ۶ تا ۱۰۰ کارکتر باشد'),
         validators.DataRequired(message=u'این قسمت اجباری است'),
-        validators.Email(message=u'آدرس پست الکترونیک اشتباه است'),
+        validators.Email(message=u'نشانی پست الکترونیک اشتباه است'),
     ])
 
     nickname = StringField('Nickname', [
         validators.Optional(),
-        validators.Length(min=3, max=80, message=u'طول نام پرداخت کننده باید بین ۳ تا ۸۰ کارکتر باشد')
+        validators.Length(min=3, max=80, message=u'طول نام پرداخت‌کننده باید بین ۳ تا ۸۰ کارکتر باشد')
     ])
 
     amount = StringField('Amount', [
         validators.DataRequired(message=u'این قسمت اجباری است و از اعداد انگلیسی استفاده کنید.'),
         # validators.Regexp(regex='^[1-9][0-9]{3,}$')
-        validators.Regexp(regex='^[2-9][0-9]{3,}$', message=u'حداقل مبلغ پرداختی ۲۰۰۰ تومان می باشد')
+        validators.Regexp(regex='^[2-9][0-9]{3,}$', message=u'حداقل مبلغ پرداختی ۲۰۰۰ تومان است')
     ])
