5626d4b63fa194c8d5255ddf4aef4873baa1005e
Hamid FzM <hamidfzm@gmail.com>
2014-11-12 19:46:52 +0330

    updating noreply mail and start using env variables

--- config.py
+++ config.py
@@ -4,8 +4,6 @@
 import os
 from datetime import datetime
 
-# flask import
-# from flask.ext.babel import gettext as _
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,14 +14,14 @@ class Config:
 
     CAMPAIGN_START = datetime(2014, 10, 19, 0, 0)
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY')
-    MMERCHANT_ID = os.environ.get('MMERCHANT_ID')
+    SECRET_KEY = os.environ.get('SECRET_KEY_PFONT')
+    WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY_PFONT')
+    MMERCHANT_ID = os.environ.get('MMERCHANT_ID_PFONT')
     ZARINPAL_WEBSERVICE = 'https://de.zarinpal.com/pg/services/WebGate/wsdl'
 
     # mandrill config
-    MANDRILL_API_KEY = os.environ.get('MANDRILL_API_KEY')  # use to send mail using mandrill service
-    NO_REPLY_MAIL_ADDRESS = 'noreply@librefont.ir'
+    MANDRILL_API_KEY = os.environ.get('MANDRILL_API_KEY_PFONT')  # use to send mail using mandrill service
+    NO_REPLY_MAIL_ADDRESS = 'noreply@pfont.ir'
     NO_REPLY_MAIL_NAME = 'کمپین قلم فارسی آزاد'
     GOOGLE_ANALYTICS_DOMAINS = ['librefont.ir', 'www.librefont.ir', 'pfont.ir', 'www.pfont.ir']
     EXCLUDED_DOMAINS = ['librefont.ir', 'pfont.ir']  # if site where under construction these domains will be excluded
@@ -33,25 +31,17 @@ class Config:
     FLASKY_MAIL_SENDER = 'Hamid FzM <mail@hamidfzm.ir>'
     FLASKY_ADMIN = 'Hamid FzM'
 
-    # LANGUAGES = {
-    #     'en': 'English',
-    #     'fa': u'فارسی'
-    # }
+    LANGUAGES = {
+        'en': 'English',
+        'fa': u'فارسی'
+    }
+
     BABEL_DEFAULT_LOCALE = 'fa'
     BABEL_DEFAULT_TIMEZONE = 'UTC+03:30'
 
-    # SMS Configs
-    SMS_SENDER = ''
-    SMS_USERNAME = ''
-    SMS_PASSWORD = ''
-    SMS_SOAP_SERVICE = ''
-
-    # smtp mail sender config
-    # MAIL_SERVER = 'smtp.googlemail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MONGODB_SETTINGS = {'DB': 'pfont',
+                    'HOST': '127.0.0.1',
+                    'PORT': 27017}
 
     @staticmethod
     def init_app(app):
@@ -60,21 +50,12 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MONGODB_SETTINGS = {'DB': 'pfont',
-                        'HOST': '127.0.0.1',
-                        'PORT': 27017}
     MINIFY_PAGE = False
     UNDER_CONSTRUCTION = False
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    MONGODB_SETTINGS = {'DB': 'pfont',
-                        # TODO set username and password for mongo learnhub database
-                        # 'USERNAME': 'my_user_name',
-                        # 'PASSWORD': 'my_secret_password',
-                        'HOST': '127.0.0.1',
-                        'PORT': 27017}
     MINIFY_PAGE = True
 
     UNDER_CONSTRUCTION = False
